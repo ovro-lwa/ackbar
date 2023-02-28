b96ef1a7d853850ff5ebc66eb602a507cc866b4b
Yuping Huang <yupinghuang@users.noreply.github.com>
2023-02-27 20:08:30 -0800

    Fetch file and stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 use plotly::{Plot, Scatter, ScatterPolar};
 use yew::prelude::*;
+use gloo_net::http::Request;
 
 #[function_component]
 fn App() -> Html {
+    let results = use_state(|| String::new());
+    {
+        let results = results.clone();
+        use_effect_with_deps(move |_| {
+            let results = results.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_videos: String = Request::get("/~yuping/more.txt")
+                    .send()
+                    .await
+                    .unwrap()
+                    .text()
+                    .await
+                    .unwrap();
+                results.set(fetched_videos.clone());
+            });
+            || ()
+        }, ());
+    }
     let counter = use_state(|| 0);
     let onclick = {
         let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
+        Callback::from(move |_| counter.set(*counter + 1))
     };
 
     html! {
         <div>
             <button {onclick}>{ "+1" }</button>
             <p>{ *counter }</p>
+            <p>{ (*results).clone() }</p>
         <PlotComponent />
         </div>
     }
